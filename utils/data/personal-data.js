723e42656c281250f2edb38359452700dd643e78
sumitchapagai <76559741+sumitchapagai@users.noreply.github.com>
2024-07-13 15:42:46 +0545

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Resham Neupane",
-  profile: '/resham.png',
+  profile: '/449604494_1464730284405103_1315467814055184074_n.png',
   designation: "Software Developer",
   description: "My name is Resham Neupane. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'reshamneupane8848@gmail.com',
